068f6780bdac561df63b27b7b3211fb7ad6f326a
Aditya Kumar T <aditya.anil950@gmail.com>
2026-06-06 12:55:40 +0530

    feat: add server-side rendering support and update dependencies
    - Updated package.json to include Angular SSR dependencies and Express.
    - Created app.config.server.ts to merge application and server configurations.
    - Added app.routes.server.ts to define server-side routes with prerendering.
    - Modified theme.service.ts to handle platform checks for SSR compatibility.
    - Implemented main.server.ts for bootstrapping the Angular application on the server.
    - Enhanced main.ts to include client hydration support.
    - Developed server.ts to set up an Express server for handling requests and serving static files.
    - Updated tsconfig.app.json to include server-related TypeScript files and node types.

--- src/app.config.ts
+++ src/app.config.ts
@@ -1,6 +1,10 @@
 // src/app/app.config.ts
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, provideZonelessChangeDetection } from '@angular/core';
 import { provideIndexedDb, DBConfig } from 'ngx-indexed-db';
+import { provideRouter, withPreloading, NoPreloading } from '@angular/router';
+import { provideHttpClient, withFetch } from '@angular/common/http';
+import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+import { AppRoutes } from './app/app-routing.module';
 
 const dbConfig: DBConfig = {
   name: 'ExperienceCalcDB',
@@ -19,6 +23,13 @@ const dbConfig: DBConfig = {
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideIndexedDb(dbConfig)
+    provideIndexedDb(dbConfig),
+    provideZonelessChangeDetection(),
+    provideHttpClient(withFetch()),
+    provideRouter(
+      AppRoutes,
+      withPreloading(NoPreloading)
+    ),
+    provideClientHydration(withEventReplay())
   ]
 };
