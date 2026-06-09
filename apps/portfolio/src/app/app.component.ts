be530516d1dab670a79e34f779d931b7366f79b4
Aditya Kumar T <aditya.anil950@gmail.com>
2026-06-09 23:39:00 +0530

    feat: initialize Nx monorepo structure with Angular portfolio app and Firebase integration dependencies

--- apps/portfolio/src/app/app.component.ts
+++ apps/portfolio/src/app/app.component.ts
@@ -56,6 +56,11 @@ export class AppComponent {
       label: 'JSON to TypeScript',
       icon: 'code',
     },
+    {
+      link: '/user',
+      label: 'User',
+      icon: 'account_circle',
+    },
     // {
     //   link: "/formbuilder",
     //   label: "Dynamic Form",
